658feedefd5259b3f42beb494bf63cf587f2c2a2
Ziaur Rab <rabziaur77@outlook.com>
2026-08-01 23:41:00 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Interior_frontend/',
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
